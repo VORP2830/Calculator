4a0eec39fe6f3179672222ad9784d3c73c64943e
Victor Osório Ramos Porto <victor.porto@mpse.mp.br>
2024-05-09 08:51:39 -0300

    Finishing application

--- lib/pages/calculator_page.dart
+++ lib/pages/calculator_page.dart
@@ -2,6 +2,7 @@ import 'package:calculator/models/memory.dart';
 import 'package:calculator/widgets/display_widget.dart';
 import 'package:calculator/widgets/keyboard_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class CalculatorPage extends StatefulWidget {
   const CalculatorPage({super.key});
@@ -17,9 +18,11 @@ class _CalculatorPageState extends State<CalculatorPage> {
       memory.applyCommand(text);
     });
   }
-
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]);
     return MaterialApp(
       home: Column(
         children: <Widget>[
